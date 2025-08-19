18a9bc56a311a7414d81426d70ebd1d144030dae
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-19 12:13:58 +0530

    Create HacjerRank_Bubble_sort.cpp

---
+++ HacjerRank_Bubble_sort.cpp
@@ -0,0 +1,16 @@
+void countSwaps(vector<int> a) {
+     int count = 0 ; 
+    for(int i = 0 ; i < a.size() ; i++){
+       
+        for(int j = 0 ; j < a.size() - 1; j++){
+            if(a[j] > a[j+1]){
+                swap(a[j] , a[j+1]);
+                count ++;
+            }
+        }
+       
+    }
+        cout << "Array is sorted in " << count << " swaps." << endl;
+         cout << "First Element: " << a[0] << endl;
+        cout << "Last Element: " << a[a.size() -1] << endl;
+}
