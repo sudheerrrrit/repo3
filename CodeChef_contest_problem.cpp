7f29abf3866a900155bc1e1b53554fc54294dc24
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-27 21:10:43 +0530

    Create CodeChef_contest_problem.cpp

---
+++ CodeChef_contest_problem.cpp
@@ -0,0 +1,19 @@
+
+	// your code goes here
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+        int usable_syrup = min(A, B / 2);
+        cout << 3 * usable_syrup << "\n";
+    }
+    return 0;
+}
