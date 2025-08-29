e168957f62a7ed0dcc16d1f58f903f99e2ee501e
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-29 22:55:11 +0530

    Create LeetCode_2614.cpp

---
+++ LeetCode_2614.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool CheckPrime(int n){
+        if(n == 1)return false;
+        for(int i = 2 ; i <= sqrt(n) ; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int diagonalPrime(vector<vector<int>>& nums) {
+        int maxi = 0 ; 
+        int n  = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            if(CheckPrime(nums[i][i])){
+                maxi = max(maxi , nums[i][i]);
+            }
+            if(CheckPrime(nums[i][n - i - 1])){
+                maxi = max(maxi , nums[i][n - i -1]);
+            }
+        }
+        return maxi;
+    }
+};
