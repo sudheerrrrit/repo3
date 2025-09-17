d4d271ada431519bfbd33ee1a413c520a55b29e7
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-17 10:09:09 +0530

    Create 3622.cpp

---
+++ 3622.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        if (n < 10) return false;
+
+        long long sum = 0;
+        long long prod = 1;
+        int temp = n;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            prod *= digit;
+            temp /= 10;
+        }
+
+        long long s = sum + prod;
+        if (s == 0) return false; 
+
+        return (n % s) == 0; 
+    }
+};
