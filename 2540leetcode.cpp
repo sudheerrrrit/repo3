f2ebf3142066c530dc05cf89636f05a69e729488
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-19 08:40:47 +0530

    Create 2540leetcode.cpp

---
+++ 2540leetcode.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        for(int i = 0 ; i < nums1.size() ; i++){
+            for(int j = i ; j < nums2.size() ; j++){
+                if(nums1[i]==nums2[j]){
+                    return nums1[i];
+                }
+            }
+        }
+        return -1;
+    }
+};
