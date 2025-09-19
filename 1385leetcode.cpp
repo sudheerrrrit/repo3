d018059af4bcd6c6cb08ef3c138354eb2abbde06
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-19 08:05:54 +0530

    Create 1385leetcode.cpp

---
+++ 1385leetcode.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int count = 0 ; 
+        bool valid;
+        for(int i =0 ; i < arr1.size() ; i++){
+            valid = true;
+            for(int j = 0 ; j < arr2.size() ; j++){
+                if(abs(arr1[i]-arr2[j])<=d){
+                    valid = false;
+                    break;
+                }
+            }
+            if(valid){
+                count ++;
+            }
+        }
+        return count;
+    }
+};
