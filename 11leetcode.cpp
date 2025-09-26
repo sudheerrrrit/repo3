b614a311b29a4ad6f0a23936c36372828101ccfe
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-26 15:17:58 +0530

    Create 11leetcode.cpp

---
+++ 11leetcode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0 ; 
+        int right = height.size() - 1;
+        int l , b ;
+        int area = 0 ;
+        int newarea= 0;
+        while(left < right){
+          int b = right - left ; 
+          int l = min(height[left], height[right]);
+           newarea = l* b;
+          area = max(area ,newarea );
+          if(height[left]<height[right]){
+            left++;
+          }
+          else{
+            right--;
+          }
+        }
+        return area;
+    }
+};
